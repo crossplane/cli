86dd11e7be0dd0d7f40c292a8b1e24bb89772729
Adam Wolfe Gordon <awg@upbound.io>
2026-05-05 09:32:07 -0600

    Import the crank CLI from crossplane/crossplane
    This commit contains the current `cmd/crank` from c/c and the supporting
    `internal/docker` package, with imports updated as necessary.
    
    Signed-off-by: Adam Wolfe Gordon <awg@upbound.io>

---
+++ cmd/crossplane/common/package.go
@@ -0,0 +1,2 @@
+// Package common provides functions and helpers shared between CLI utilities, available for import by external tools interacting with Crossplane.
+package common //nolint:revive // TODO(adamwg): Move these packages into pkg/ or internal/.
